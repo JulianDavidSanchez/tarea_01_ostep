246685bb2d70fa69313f4385bc6c3a340a0669a3
SantiagoRamirezRuiz <santiago.ramirez.ruiz@correounivalle.edu.co>
2019-06-18 10:57:11 -0500

    Subiendo io.c

--- io.c
+++ io.c
@@ -1,13 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <assert.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <string.h>
-#include <stdlib.h>
-
-
 
 int main(int argc, char *argv[])
 {
@@ -31,12 +22,11 @@ int main(int argc, char *argv[])
     h++;
    }
    int p;
-   for(p=h; p>=0; p--){
+   for(p=(h-1); p>=0; p--){
     printf("%c",mat[p]);
    }
-printf("\n");
-  
-    fclose(f);
+   printf("\n");
+   fclose(f);
   }
   return 0;
 }
